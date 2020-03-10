f772004d8028de24d248a5076f905b416f0ca3f8
Shane Droogan <shanedroogan@gmail.com>
2020-03-10 01:24:24 -0400

    checking tokenizer output

--- src/transformers/data/processors/glue.py
+++ src/transformers/data/processors/glue.py
@@ -181,11 +181,11 @@ class BoolqProcessor(DataProcessor):
 
     def get_train_examples(self, data_dir):
         """See base class."""
-        return self._create_examples(self._read_jsonl(os.path.join(data_dir, "train.jsonl")), "train")
+        return self._create_examples(self._read_json(os.path.join(data_dir, "train.jsonl")), "train")
 
     def get_dev_examples(self, data_dir):
         """See base class."""
-        return self._create_examples(self._read_jsonl(os.path.join(data_dir, "dev.jsonl")), "dev")
+        return self._create_examples(self._read_json(os.path.join(data_dir, "dev.jsonl")), "dev")
 
     def get_labels(self):
         """See base class."""
